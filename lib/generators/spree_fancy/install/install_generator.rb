c9c4ff0039a0d46fa7215f2a721183209aa27c7a
Deepak <Deepak@ONGRAPH053.(none)>
2013-06-06 19:00:37 +0530

    change initial

--- lib/generators/spree_fancy/install/install_generator.rb
+++ lib/generators/spree_fancy/install/install_generator.rb
@@ -7,6 +7,10 @@ module SpreeFancy
         append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_fancy\n"
       end
 
+      def add_controller
+        copy_file 'app/controller/spree/home_controller_decorator.rb', 'app/controller/home_controller_decorator.rb'
+      end
+
       def add_stylesheets
         inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_fancy\n", :before => /\*\//, :verbose => true
         inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_fancy\n", :before => /\*\//, :verbose => true
